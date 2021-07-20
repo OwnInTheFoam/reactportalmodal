f4a99cb14c9ebd71ccd1af88efc0a9735d59932d
Trent Cooper <t.Cooper@reeltimegaming.com>
2021-07-20 10:46:22 +1000

    Add modal to open via portal

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import Modal from './Modal';
+
+const BUTTON_WRAPPER_STYLES = {
+  position: 'relative',
+  zIndex: 1
+}
+
+const OTHER_CONTENT_STYLES = {
+  position: 'relative',
+  zIndex: 2,
+  backgroundColor: 'red',
+  padding: '10px'
+}
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  return (
+    <>
+      {/* The onClick event will be passed to the modal as child parent relation*/}
+      <div style={BUTTON_WRAPPER_STYLES} onClick={()=> console.log('click')}>
+        <button onClick={()=>setIsOpen(true)}>Open Modal</button>
+        
+        <Modal open={isOpen} onClose={()=>setIsOpen(false)}>
+          Fancy Modal
+        </Modal>
+      </div>
+      
+      <div style={OTHER_CONTENT_STYLES}>Other Content</div>
+    </>
+  );
+}
